a12815b4b7b86c5af14fc56c6b2594b20ee5cbe9
flypsoff <52503674+flypsoff@users.noreply.github.com>
2020-09-14 14:39:38 +0300

    added express-session and passport 'config'

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const app = express()
+const passport = require('passport')
+const session = require('express-session')
+
+require('./config/passport')(passport)
 
 // we use this for disguise password in file .env
 dotenv.config()
@@ -18,8 +22,16 @@ app.use(express.urlencoded({extended:false}))
 
 app.set('view engine', 'ejs')
 
-app.use('/', require('./routes/user'))
+app.use(session({
+    secret: 'cat',
+    resave: false,
+    saveUninitialized: false
+}))
 
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/', require('./routes/user'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log('Server started...'))

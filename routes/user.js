6cb2750f5f6f34a2ef2a90efd20dcc06d2bb0d3e
flypsoff <52503674+flypsoff@users.noreply.github.com>
2020-09-14 14:41:13 +0300

    added some routs(login and logout but logout without 'tip')

--- routes/user.js
+++ routes/user.js
@@ -2,16 +2,18 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
+const { isNotAuthenticatedMiddleware, alreadyAuthenticatedMiddleware } = require('../config/auth')
 
-router.get('/login', (req, res) => {
+router.get('/login', alreadyAuthenticatedMiddleware, (req, res) => {
     res.render('login')
 })
 
-router.get('/register', (req, res) => {
+router.get('/register', alreadyAuthenticatedMiddleware, (req, res) => {
     res.render('register')
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', alreadyAuthenticatedMiddleware, async (req, res) => {
     const { username, email, password, password2 } = req.body
     const errors = []
     const checkUser = await User.findOne({email})
@@ -27,17 +29,18 @@ router.post('/register', async (req, res) => {
     if(password !== password2) {
         errors.push({ msg: 'Password must be the same' })
     }
+
     if(errors.length > 0) {
         res.render('register', {
             errors
-        })
+        })  
     } else {
         const newUser = new User({
             username,
             email,
             password
         })
-
+        
         try{
             const hash = await bcrypt.hash(newUser.password, 10)
             newUser.password = hash
@@ -48,8 +51,22 @@ router.post('/register', async (req, res) => {
     }
 })
 
-router.post('login', (req, res) => {
-    
+router.post('/login', alreadyAuthenticatedMiddleware, (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    })(req, res, next)
 })
+
+router.get('/logout', isNotAuthenticatedMiddleware, (req, res) => {
+    req.logout();
+    res.redirect('/login');
+});
+
+
+router.get('/', isNotAuthenticatedMiddleware, (req, res) => {
+    res.send(`<h1>Hello man</h1><br><a href="/logout">Log out</a>`)
+})
+
 
 module.exports = router
